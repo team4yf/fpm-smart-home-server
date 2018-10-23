9e17462d63843a7a5d3ed7e5b3f6d790e00197fa
yfsoftcom <yfsoftcom@126.com>
2018-10-23 18:01:14 +0800

    debug

--- fake/bot.js
+++ fake/bot.js
@@ -7,10 +7,10 @@ const client = mqtt.connect(`mqtt://${host}:${port}`, {
 });
 const sn = 'y0z0h0c0z0n0';
 setInterval( () => {
-	client.publish(`$s2d/u3/p1/${sn}/activeScene`, `2`, { qos: 1, retain: true});
+	client.publish(`$s2d/u3/p1/${sn}/activeScene`, `FOO BaR`, { qos: 1, retain: true});
 }, 15 * 1000)
 
-client.subscribe([`$s2d/u3/p1/${sn}/trunon`, `$s2d/u3/p1/${sn}/trunoff`, `$s2d/u3/p1/${sn}/activeScene`, `$s2d/u3/p1/${sn}/deactiveScene`]);
+client.subscribe([`$s2d/u3/p1/${sn}/trunon`, `$s2d/u3/p1/${sn}/trunoff`, `$s2d/u3/p1/${sn}/activeScene`, `$s2d/u3/p1/${sn}/deactiveScene`, `$s2d/u3/p1/update`]);
 client.on('message', (topic, payload) => {
     console.log('bot receive: ', topic, payload.toString());
 })
